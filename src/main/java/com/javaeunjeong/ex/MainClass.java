f7e619d949f181e1bda62f065bedb9a2bc367ec2
choieunheong <mjchoi17@hanmail.net>
2022-04-13 15:19:45 +0900

    7-2 스프링 빈 생명주기 연습

---
+++ src/main/java/com/javaeunjeong/ex/MainClass.java
@@ -0,0 +1,17 @@
+package com.javaeunjeong.ex;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		
+		ctx.load("classpath:applicationCTX.xml");//컨테이너 설정
+		ctx.refresh();//컨테이너 설정
+		
+		ctx.close();//컨테이너 종료
+	}
+
+}
